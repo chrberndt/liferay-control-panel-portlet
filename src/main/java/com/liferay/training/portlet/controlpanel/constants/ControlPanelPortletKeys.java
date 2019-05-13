4c85e95c139fce3d2f2c4c3e2381f3fbc973780f
Christian Berndt <christian.berndt@liferay.com>
2019-05-13 09:26:48 +0200

    Initial check-in of control-panel portlet.

---
+++ src/main/java/com/liferay/training/portlet/controlpanel/constants/ControlPanelPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.training.portlet.controlpanel.constants;
+
+/**
+ * @author berndt
+ */
+public class ControlPanelPortletKeys {
+
+	public static final String CONTROL_PANEL = "com_liferay_training_portlet_controlpanel_ControlPanelPortlet";
+
+}
---
+++ src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="controlpanel.caption"/></b>
+</p>
